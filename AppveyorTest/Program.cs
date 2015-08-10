33319f4cb796c25bfd2b1a440fc4b3bdc875dad4
unknown <damia01@HBMIND-DAMIAN01.CompanyNet.org>
2015-08-10 13:04:03 -0400

    Initial Commit

---
+++ AppveyorTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppveyorTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a do nothing application to test an appveyor build");
+        }
+    }
+}
