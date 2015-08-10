8d449a956b7ab97b2047a3362c050a84addfaeee
unknown <damia01@HBMIND-DAMIAN01.CompanyNet.org>
2015-08-10 13:34:11 -0400

    Add unit tests

---
+++ ApveyorTestUT/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AppveyorTest;
+
+namespace ApveyorTestUT
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            TestClass tc = new TestClass();
+            Console.WriteLine("Test Executed: " + tc.TestString);
+        }
+    }
+}
